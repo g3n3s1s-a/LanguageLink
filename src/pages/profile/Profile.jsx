7e912da90d306c81e6751cd79be88982a15a53b2
gen <genesisargueta@dhcp-10-12-238-236.dhcp.nd.edu>
2024-04-12 21:53:58 -0400

    changed names

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -25,13 +25,15 @@ export default function Profile() {
               />
             </div>
             <div className="profileInfo">
-                <h4 className="profileInfoName">Safak Kocaoglu</h4>
-                <span className="profileInfoDesc">Hello my friends!</span>
+              <h4 className="profileInfoName">Safak Kocaoglu</h4>
+              <span className="profileInfoDesc">
+                Hello my friends I want to learn Spanish!
+              </span>
             </div>
           </div>
           <div className="profileRightBottom">
             <Feed />
-            <Rightbar profile/>
+            <Rightbar profile />
           </div>
         </div>
       </div>
