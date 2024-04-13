8475e844cf18fe1707db19a4291effd557e27f50
Miguel Jose Maninang <138338993+migueljoseofficial@users.noreply.github.com>
2024-04-13 15:49:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+
 import thepath from "./routes"
+import "./global.css";
+
 
 
 function App() {
-  return (
-    <>
-    <thepath />
-    <Register />;
-    </>
-  )
+  return <Home />;
 }
 
 export default App;
