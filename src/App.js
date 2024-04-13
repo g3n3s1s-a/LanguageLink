1564b59aae0754bcfc823b5ec8f40c4262c2073f
Miguel Jose Maninang <migueljosemaninang@miguels-air.dhcp.nd.edu>
2024-04-13 15:36:48 -0400

    added some routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import thepath from "./routes"
+
 
 function App() {
-  return <Home />;
+  return (
+    <>
+    <thepath />
+    <Register />;
+    </>
+  )
 }
 
 export default App;
