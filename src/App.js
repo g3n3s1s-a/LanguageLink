8f6770a3ad64a5d334cb1160f768579bfbd34087
Emmanuel De La Paz <143834994+emandlp@users.noreply.github.com>
2024-04-13 16:04:44 -0400

    router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 import "./global.css";
-
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
