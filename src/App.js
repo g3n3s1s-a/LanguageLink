2fa5b3ea05c3c4c6e99a621cf33c41c84d802885
Emmanuel De La Paz <143834994+emandlp@users.noreply.github.com>
2024-04-13 14:35:14 -0400

    fonts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import './global.css';
 
 function App() {
   return <Home />;
